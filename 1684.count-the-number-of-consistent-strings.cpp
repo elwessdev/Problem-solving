7b0ad7ceaed0648a396d88fbf3ea974668e40549
devosema <benaliosama3@gmail.com>
2024-04-29 23:43:40 +0100

    String

--- 1684.count-the-number-of-consistent-strings.cpp
+++ 1684.count-the-number-of-consistent-strings.cpp
@@ -7,20 +7,18 @@
 // @lc code=start
 class Solution {
 public:
-    int countConsistentStrings(string allowed, vector<string>& words) {
-        bool validStr(string str, map<char,int> mp){
+    bool validStr(string str, map<char,int> mp){
         for(int i=0;i<str.size();i++){
             if(mp[str[i]]!=1) return false;
         }
         return true;
-        }
-        int countConsistentStrings(string allowed, vector<string>& words) {
+    }
+    int countConsistentStrings(string allowed, vector<string>& words) {
         int ans=0;
         map<char,int> mp;
         for(auto c:allowed) mp[c]=1;
         for(auto word:words) ans+=validStr(word,mp);
         return ans;
-    }
     }
 };
 // @lc code=end
